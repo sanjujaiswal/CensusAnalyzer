0a24b23bf166107562ec510956a752b7610a5ec3
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-05-24 11:59:41 +0530

    ADDED: Welcome message

---
+++ CensusAnalyzerMain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CensusAnalyzerMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian States Census Analyser Program!!");
+        }
+    }
+}
